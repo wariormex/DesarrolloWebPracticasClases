a4251e9fb477a506d464281fee615b5f80070b05
wariormex <wariormex@hotmail.com>
2022-08-19 15:39:34 -0500

    Initial commit

---
+++ js/05_practica5.js
@@ -0,0 +1,6 @@
+console.log("Verificando ejecucion JS...;")
+alert("Prueba de alert;")
+document.title = 'Nuevo titulo';
+//document.body = 'asdfghjsadf' //Error
+//document.body.innerHTML = "Esta es mi página";
+document.body.innerHTML += "<p> parrafo 1 </p>";
